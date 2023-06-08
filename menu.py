1cbe7692281a2c6945c764b86d3c963cc09565ef
NeverSetPy <127951630+NeverSetPy@users.noreply.github.com>
2023-06-08 18:56:34 +0100

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,11 @@
+item1 = input('Choose one favourite food from the menu:')
+item2 = input('Choose a second favourite food:')
+item3 = input('Choose a third favourite food:')
+
+print('Here is your order:')
+
+print(item1)
+print(item2)
+print(item3)
+
+print('Thank you. Delivery will be 2-3 hours. ')
